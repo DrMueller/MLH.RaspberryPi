e6373894f397fca3bdd688f03ac09b6fa9a5e957
Matthias Müller <MatthiasM@Live.de>
2019-09-07 22:46:43 +0200

    WIP

---
+++ Sources/Application/Areas/SenseHats/Models/SenseHat.cs
@@ -0,0 +1,18 @@
+﻿using System.Device.Gpio;
+using System.Device.I2c;
+
+namespace Mmu.Mlh.RaspberryPi.Areas.SenseHats.Models
+{
+    internal class SenseHat
+    {
+        private const int DeviceAddress = 0x46;
+
+        public SenseHat()
+        {
+            var controller = new GpioController(PinNumberingScheme.Board);
+
+            var tra = new I2cConnectionSettings(2, DeviceAddress);
+            var con = I2cDevice.Create(tra);
+        }
+    }
+}
