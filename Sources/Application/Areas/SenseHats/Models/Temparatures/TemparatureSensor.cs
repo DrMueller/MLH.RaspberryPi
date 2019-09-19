a591c906d04939191adc854d79f3046fbbdcae1b
Matthias Müller <matthiasm@live.de>
2019-09-19 20:38:25 +0200

    WIP

--- Sources/Application/Areas/SenseHats/Models/Temparatures/TemparatureSensor.cs
+++ Sources/Application/Areas/SenseHats/Models/Temparatures/TemparatureSensor.cs
@@ -4,6 +4,10 @@ using Mmu.Mlh.RaspberryPi.Infrastructure.PythonAccess.Services;
 
 namespace Mmu.Mlh.RaspberryPi.Areas.SenseHats.Models.Temparatures
 {
+    /// <summary>
+    /// For some reason, this currently doesn't work, but just on the PI
+    /// See also here: https://stackoverflow.com/questions/57980163/python-called-via-c-sharp-not-returning-value-when-in-infinite-loop
+    /// </summary>
     public class TemparatureSensor : Device
     {
         internal TemparatureSensor(IPythonExecutor executor, string scriptFilePath) : base(executor, scriptFilePath)
